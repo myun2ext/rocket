c19ad74c2397a2b649f421b5b16a0c1306070321
myun2 <myun2@nwhite.info>
2014-10-30 17:00:23 +0900

    http.c add

---
+++ http.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "rocket.h"
+#include "rocket/http.h"
+
+ROCKET http_rocket(const char* host)
+{
+	ROCKET rock = tcp_rocket();
+	fire_tcp_rocket(rock, host, 80);
+	return rock;
+}
+
+unsigned int get_http_data(const char* host, unsigned short port, const char* path, char* buffer, unsigned int bufsize)
+{
+	ROCKET rock = tcp_rocket();
+	fire_tcp_rocket(rock, host, port);
+
+	send(rock, "GET ", 4, 0);
+	send(rock, path, strlen(path), 0);
+	send(rock, "\r\n ", 2, 0);
+
+	send(rock, "HTTP/1.1\r\n", 10, 0);
+	send(rock, "Host: ", 6, 0);
+	send(rock, host, strlen(host), 0);
+	send(rock, "\r\n ", 2, 0);
+
+	return recv(rock, buffer, bufsize, 0);
+}
