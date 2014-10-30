619d81ab8139f77839d943bce261fee7d2167087
myun2 <myun2@nwhite.info>
2014-10-30 17:36:13 +0900

    reciever

--- reciever.c
+++ reciever.c
@@ -1,9 +1,26 @@
 void rocket_reciever(const ROCKET_RECIEVER_SETTINGS* settings)
 {
-	ROCKET rock = rocket();
-	rocket_bind(rock, settings->port);
-	rocket_listen(rock, settings->max_connections);
+	ROCKET listen_rock = rocket(settings->socket_family, settings->socket_type, settings->protocol);
+	listen_rocket(settings->port, settings->max_queing);
 
-	while (1)
-		settings->callback(rocket_accept(sock));
+	while(1)
+		settings->callback(rocket_accept(listen_rock));
+}
+
+void tcp_rocket_reciever(const ROCKET_RECIEVER_SETTINGS* settings)
+{
+	ROCKET listen_rock = rocket(AF_INET, SOCK_STREAM, 0);
+	listen_rocket(settings->port, settings->max_queing);
+
+	while(1)
+		settings->callback(rocket_accept(listen_rock));
+}
+
+void http_rocket_reciever(const ROCKET_RECIEVER_SETTINGS* settings)
+{
+	ROCKET listen_rock = rocket(AF_INET, SOCK_STREAM, 0);
+	listen_rocket(80, settings->max_queing);
+
+	while(1)
+		settings->callback(rocket_accept(listen_rock));
 }
--- rocket.h
+++ rocket.h
@@ -17,9 +17,23 @@
 
 ROCKET rocket(int socket_family, int socket_type, int protocol);
 struct sockaddr_in rocket_address(const char* host, unsigned short port);
-void fire_rocket(ROCKET rock, const char* host, unsigned short port);
+void fire_tcp_rocket(ROCKET rock, const char* host, unsigned short port);
 void fire_udp_rocket(ROCKET rock, const char* host, unsigned short port);
 void destroy_rocket(ROCKET rock);
 ROCKET listen_rocket(ROCKET rock, unsigned short port, int max_queing);
+
+#define tcp_rocket()	rocket(AF_INET, SOCK_STREAM, 0)
+#define udp_rocket()	rocket(AF_INET, SOCK_DGRAM, 0)
+
+
+/* reciever */
+typedef struct
+{
+	int socket_family, socket_type, protocol;
+	int port, max_queing;
+	void (*callback)(ROCKET);
+} ROCKET_RECIEVER_SETTINGS;
+
+void rocket_reciever(const ROCKET_RECIEVER_SETTINGS* settings);
 
 #endif//__MYUN2_GITHUB_ROCKET__ROCKET_H__
