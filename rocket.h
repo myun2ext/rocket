7808d02ca1f17023e2f302147d1f5fe0e849afca
myun2 <myun2@nwhite.info>
2014-10-30 12:16:55 +0900

    include headers

--- rocket.h
+++ rocket.h
@@ -1,4 +1,16 @@
 #ifndef __MYUN2_GITHUB_ROCKET__ROCKET_H__
 #define __MYUN2_GITHUB_ROCKET__ROCKET_H__
 
+#ifdef WIN32
+	#include <ws2tcpip.h>
+	#include <Wspiapi.h>
+	#include <winsock2.h>
+#else
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
+#endif
+
 #endif//__MYUN2_GITHUB_ROCKET__ROCKET_H__
--- win32.c
+++ win32.c
@@ -1,4 +1,1 @@
-#include <ws2tcpip.h>
-#include <Wspiapi.h>
-#include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
